e854aaa08e43bb9388265d406cdc5eee5e7ac055
hiep <vanhiep2102@gmail.com>
2021-08-15 00:44:15 +0700

    create design trello fe

--- trello-web/src/App.js
+++ trello-web/src/App.js
@@ -1,9 +1,107 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="trello">
+      <div className="navbar app">App </div>
+      <div className="navbar board">Board Bar</div>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://phunugioi.com/wp-content/uploads/2020/10/anh-dai-dien-nguoi-giau-mat-nam.jpg" alt="hiepDev" />
+              Title: HiepDev
+            </li>
+            <li>Add  what you'd like to work on below</li>
+            <li>Add  what you'd like to work on below</li>
+            <li>Add  what you'd like to work on below</li>
+            <li>Add  what you'd like to work on below</li>
+            <li>Add  what you'd like to work on below</li>
+            <li>Add  what you'd like to work on below</li>
+            <li>Add  what you'd like to work on below</li>
+            
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://phunugioi.com/wp-content/uploads/2020/10/anh-dai-dien-nguoi-giau-mat-nam.jpg" alt="hiepDev" />
+              Title: HiepDev
+            </li>
+            <li>Add  what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://phunugioi.com/wp-content/uploads/2020/10/anh-dai-dien-nguoi-giau-mat-nam.jpg" alt="hiepDev" />
+              Title: HiepDev
+            </li>
+            <li>Add  what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://phunugioi.com/wp-content/uploads/2020/10/anh-dai-dien-nguoi-giau-mat-nam.jpg" alt="hiepDev" />
+              Title: HiepDev
+            </li>
+            <li>Add  what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://phunugioi.com/wp-content/uploads/2020/10/anh-dai-dien-nguoi-giau-mat-nam.jpg" alt="hiepDev" />
+              Title: HiepDev
+            </li>
+            <li>Add  what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://phunugioi.com/wp-content/uploads/2020/10/anh-dai-dien-nguoi-giau-mat-nam.jpg" alt="hiepDev" />
+              Title: HiepDev
+            </li>
+            <li>Add  what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://phunugioi.com/wp-content/uploads/2020/10/anh-dai-dien-nguoi-giau-mat-nam.jpg" alt="hiepDev" />
+              Title: HiepDev
+            </li>
+            <li>Add  what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://phunugioi.com/wp-content/uploads/2020/10/anh-dai-dien-nguoi-giau-mat-nam.jpg" alt="hiepDev" />
+              Title: HiepDev
+            </li>
+            <li>Add  what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
