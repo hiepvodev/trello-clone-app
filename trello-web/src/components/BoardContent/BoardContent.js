4ab575782bce7a7ce6aecfe5362de525251e8352
hiep <vanhiep2102@gmail.com>
2021-08-16 10:05:31 +0700

    initial sample data

--- trello-web/src/components/BoardContent/BoardContent.js
+++ trello-web/src/components/BoardContent/BoardContent.js
@@ -1,16 +1,32 @@
 import Column from 'components/Column/Column';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './BoardContent.scss'
+import { initialData } from 'actions/initialData'
+import { isEmpty } from 'lodash'
+import { mapOrder } from 'utilities/sorts'
+
 
 function BoardContent(props) {
+    const [ board, setBoard] = useState({})
+    const [columns, setColumns] = useState([])
+
+    useEffect(() => {
+        const boardFromDB = initialData.boards.find(board => board.id === 'board-1')
+        if(boardFromDB){
+            setBoard(boardFromDB)
+
+            //sort column
+            setColumns(mapOrder(boardFromDB.columns,boardFromDB.columnOrder,'id'))
+        }
+    },[])
+
+    if(isEmpty(board)){
+        return <div className='not-found' style={{padding: '10px', color: 'white'}}>Board not found</div>
+    }
+
     return (
         <div className="board-content">
-            <Column/>
-            <Column/>
-            <Column/>
-            <Column/>
-            <Column/>
-            <Column/>
+            {columns.map((column, index) => <Column  key={index} column={column}/>)}
         </div>
     );
 }
