c9be0bcf9be061541af32d1d6d446842f4952171
hiep <vanhiep2102@gmail.com>
2021-08-17 00:46:04 +0700

    handle state change

--- trello-web/src/App.js
+++ trello-web/src/App.js
@@ -2,6 +2,7 @@ import './App.scss'
 import AppBar from 'components/AppBar/AppBar'
 import BoardBar from 'components/BoardBar/BoardBar'
 import BoardContent from 'components/BoardContent/BoardContent'
+import React from 'react'
 
 function App() {
   return (
